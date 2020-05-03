07cd37990edfa02f22d161ee2f883acc836fae97
Isabella Guimet <isabellaguimet@hotmail.com>
2020-05-03 16:32:59 -0600

    Added documentation, cleaned up code

--- Buzz.java
+++ Buzz.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
+// By: Isabella Guimet 
+
+// Fully funtional simulation option
+// Game play - user only allowed to enter numbers, program inserts "buzz" when needed 
+
 public class Buzz {
 
-    // Not completed yet - user needs to enter buzz correctly - string input instead of int
-
-    public static String intToString(int num) {
-        String str = String.valueOf(num);
-        return str;
-    }
-
-    /* Checks that the user's input is the next num expected i.e. one more than the previous num
-     *
+    /**
+     * Checks that the user's input is the next num expected i.e. one more than the previous num
+     * 
      * @param previous_num   the number before the number the user will enter
      * @param user_num       the number the user will enter
+     * @return               returns true if user_num is the next number expected. Otherwise, returns false.
      * 
-     * @return Boolean       returns true if user_num is the next number expected. Otherwise, returns false.
      */
 
     public static Boolean check(int previous_num, int user_num) {
@@ -27,24 +26,25 @@ public class Buzz {
         }
     }
 
-    /* This simulation automatically generates all numbers with the correct pattern specified.
-    * 
-    * @param max            the maximum number this simulation goes up to
-    * @param rule_pattern   the integer number this simulation says buzz for            
-    *
-    * @return String        returns "End of Simulation" when done
-    */
+    /**
+     * This simulation automatically generates all numbers with the correct pattern specified.
+     * 
+     * @param max            the maximum number this simulation goes up to 
+     * @param rule_pattern   the integer number this simulation says buzz for
+     * @return               returns "End of Simulation" when done
+     * 
+     */
 
     public static String Simulation (int max, int rule_pattern) {
         
         // number is divisible by 7 OR number has a '7' in it
         int num = 1;
 
-        String str2 = intToString(rule_pattern);
+        String str2 = Integer.toString(rule_pattern);
 
         while (num <= max) {
 
-            String str1 = intToString(num);
+            String str1 = Integer.toString(num); 
 
             if (num % rule_pattern == 0 || str1.indexOf(str2) != -1) {
                 System.out.println("Buzz");
@@ -60,6 +60,13 @@ public class Buzz {
         return "End of Simulation"; 
     }
 
+    /**
+     * Main execution of program. The user can choose to play the simulation option or the game play
+     * option.
+     * 
+     * @param args
+     */
+
     public static void main(String[] args) {
         int num = 0;
         boolean input = true;
@@ -67,9 +74,11 @@ public class Buzz {
         Scanner scn3 = new Scanner(System.in);
         System.out.println("Would you like to play through the game or run a simulation?");
         System.out.println("Type 'play' to play through the game or type 'run' to run a simulation.");
+        
         String program_type = scn3.next();
         String s_run = "run";
         String s_play = "play";
+        System.out.println("");
 
         if (program_type.compareToIgnoreCase(s_run) == 0) {
             Scanner scner = new Scanner(System.in);
@@ -84,16 +93,19 @@ public class Buzz {
         if (program_type.compareToIgnoreCase(s_play) == 0) {
 
             Scanner scn = new Scanner(System.in);
-            System.out.println("Enter your number starting from 1: ");
+            System.out.println("Note: You won't be allowed to enter the word \"Buzz\", only the numbers");     
+            System.out.println("");
+            System.out.println("Enter your number starting from 1: ");   
+            
             int user_input = scn.nextInt();
             input = check(num, user_input);
             
 
-            while (input == true) {
+            while (input == true) {         
+
+                String str1 = Integer.toString(user_input);
+
                 // number is divisible by 7 OR number has a '7' in it
-
-                String str1 = intToString(user_input);
-
                 if (user_input % 7 == 0 || str1.indexOf('7') != -1) {
                     System.out.println("Buzz");
                 }
@@ -101,14 +113,12 @@ public class Buzz {
                 num = num + 1;
 
                 Scanner scn1 = new Scanner(System.in);
-                //System.out.println("");
                 user_input = scn1.nextInt();
                 input = check(num, user_input);
                 
             }
 
             scn.close();
-            //scn1.close();
             scn3.close();
 
             if (input == false) {
